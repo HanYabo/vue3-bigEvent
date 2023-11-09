dfbc0394e285194eefebead3a1b82a8f7e05648c
hanyabo <2247561875@qq.com>
2023-11-09 23:13:51 +0800

    个人资料重置密码-校验规则实现

--- src/views/User/UserPwd.vue
+++ src/views/User/UserPwd.vue
@@ -7,13 +7,13 @@
         <!-- 表单 -->
         <el-form :model="pwdForm" :rules="pwdFormRules" ref="pwdFormRef" label-width="100px">
             <el-form-item label="原密码" prop="oldPwd">
-                <el-input type="password"></el-input>
+                <el-input v-model="pwdForm.oldPwd" type="password"></el-input>
             </el-form-item>
             <el-form-item label="新密码" prop="newPwd">
-                <el-input type="password"></el-input>
+                <el-input v-model="pwdForm.newPwd" type="password"></el-input>
             </el-form-item>
             <el-form-item label="确认新密码" prop="rePwd">
-                <el-input type="password"></el-input>
+                <el-input v-model="pwdForm.rePwd" type="password"></el-input>
             </el-form-item>
             <el-form-item>
                 <el-button type="primary">修改密码</el-button>
@@ -31,6 +31,25 @@ const pwdForm = ref({
     newPwd: '',
     rePwd: ''
 })
+
+// 新旧密码不能相同
+const checkPwd = (rule, value, callback) => {
+    if (value === pwdForm.value.oldPwd) {
+        callback(new Error('新旧密码不能相同'))
+    } else {
+        callback() // 通过校验
+    }
+}
+
+// 确认密码和新密码要一致
+const repeatPwd = (rule, value, callback) => {
+    if(value !== pwdForm.value.newPwd) {
+        callback(new Error('两次输入的密码不相同'))
+    }else {
+        callback()
+    }
+}
+
 // 标签对象
 const pwdFormRef = ref(null)
 // 规则对象
@@ -41,11 +60,13 @@ const pwdFormRules = ref({
     ],
     newPwd: [
         { required: true, message: '请输入新密码', trigger: 'blur'},
-        { pattern: /^\S{6,12}$/, message: '密码长度必须是6-12位的非空字符串', trigger: 'blur'}
+        { pattern: /^\S{6,12}$/, message: '密码长度必须是6-12位的非空字符串', trigger: 'blur'},
+        { validator: checkPwd, trigger: 'blur'}
     ],
     rePwd: [
         { required: true, message: '请再次输入新密码', trigger: 'blur'},
-        { pattern: /^\S{6,12}$/, message: '密码长度必须是6-12位的非空字符串', trigger: 'blur'}
+        { pattern: /^\S{6,12}$/, message: '密码长度必须是6-12位的非空字符串', trigger: 'blur'},
+        { validator: repeatPwd, trigger: 'blur'}
     ]
 })
 </script>
