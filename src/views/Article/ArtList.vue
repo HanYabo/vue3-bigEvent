40b40e53f71b3d59e6be59b14ab9e737aaf45f15
hanyabo <2247561875@qq.com>
2023-11-10 23:51:15 +0800

    文章管理发表文章-表单的准备

--- src/views/Article/ArtList.vue
+++ src/views/Article/ArtList.vue
@@ -3,14 +3,14 @@
         <el-card class="box-card">
             <div slot="header" class="clearfix">
                 <span>文章列表</span>
-                <el-divider></el-divider>
+                <el-divider />
             </div>
             <!-- 搜索区域 -->
             <div class="search-box">
                 <el-form :inline="true">
                     <el-form-item label="文章分类">
                         <el-select placeholder="请选择分类" size="small">
-                            <el-option></el-option>
+                            <el-option v-for="item in cateList" :key="item.id" :label="item.name" :value="item.id"></el-option>
                         </el-select>
                     </el-form-item>
                     <el-form-item label="发布状态" style="margin-left: 15px">
@@ -25,11 +25,11 @@
                     </el-form-item>
                 </el-form>
                 <!-- 发表文章的按钮 -->
-                <el-button type="primary" size="small" class="btn-pub">发表文章</el-button>
+                <el-button type="primary" size="small" class="btn-pub" @click="showPubDialog">发表文章</el-button>
             </div>
 
             <!-- 文章表格区域 -->
-            <el-table data="" style="width: 100%;" border stripe>
+            <!-- <el-table data="" style="width: 100%;" border stripe>
                 <el-table-column label="文章标题" prop="title">
                     <template v-slot="scope">
                         <el-link type="primary"></el-link>
@@ -48,67 +48,108 @@
                         <el-button type="danger" size="mini">删除</el-button>
                     </template>
                 </el-table-column>
-            </el-table>
+            </el-table> -->
             <!-- 分页区域 -->
             <!-- <el-pagination @size-change="handleSizeChangeFn" @current-change="handleCurrentChangeFn"
                 :current-page.sync="q.pagenum" :page-sizes="[2, 3, 5, 10]" :page-size.sync="q.pagesize"
                 layout="total, sizes, prev, pager, next, jumper" :total="total"></el-pagination> -->
         </el-card>
         <!-- 发表文章的 Dialog 对话框 -->
-        <el-dialog title="发表文章" :visible.sync="pubDialogVisible" fullscreen :before-close="handleClose" @close="closeFn">
-            <el-form :model="pubForm" :rules="pubFormRules" ref="pubFormRef" label-width="100px">
+        <el-dialog title="发表文章" v-model="pubDialogVisible" fullscreen :before-close="handleClose">
+            <el-form :model="pubForm" :rules="pubFormRules" ref="" label-width="100px">
                 <el-form-item label="文章标题" prop="title">
                     <el-input v-model="pubForm.title" placeholder="请输入标题"></el-input>
                 </el-form-item>
                 <el-form-item label="文章分类" prop="cate_id">
-                    <el-select v-model="pubForm.cate_id" placeholder="请选择分类" style="width: 100%">
-                        <el-option v-for="obj in fenleiList" :label="obj.cate_name" :key="obj.id"
-                            :value="obj.id"></el-option>
-
+                    <el-select placeholder="请选择分类" style="width: 100%">
+                        <el-option v-for="item in cateList" :key="item.id" :label="item.name" :value="item.id"></el-option>
                     </el-select>
                 </el-form-item>
                 <el-form-item label="文章内容" prop="content">
-                    <quill-editor @blur="wenzhangFn" class="ql-editor" v-model="pubForm.content"></quill-editor>
+                    <quill-editor class="ql-editor"></quill-editor>
                 </el-form-item>
                 <el-form-item label="文章封面" prop="cover_img">
                     <!-- 用来显示封面的图片 -->
-                    <img src="../../assets/images/cover.jpg" alt="" class="cover-img" ref="imgRef" />
+                    <img src="../../assets/images/cover.jpg" alt="" class="cover-img" ref="" />
                     <br />
                     <!-- 文件选择框，默认被隐藏 -->
-                    <input type="file" @change="coverFn" style="display: none;" accept="image/*" ref="iptFileRef" />
+                    <input type="file" style="display: none;" accept="image/*" ref="" />
                     <!-- 选择封面的按钮 -->
-                    <el-button type="text" @click="xuanzetupianFn">+ 选择封面</el-button>
+                    <el-button type="text">+ 选择封面</el-button>
                 </el-form-item>
                 <el-form-item>
-                    <el-button type="primary" @click="pubArticleFn('已发布')">发布</el-button>
-                    <el-button type="info" @click="pubArticleFn('草稿')">存为草稿</el-button>
+                    <el-button type="primary">发布</el-button>
+                    <el-button type="info">存为草稿</el-button>
                 </el-form-item>
             </el-form>
-        </el-dialog>
-        <el-dialog title="文章预览" :visible.sync="detailVisible" width="80%">
-            <h1 class="title">{{ artDetail.title }}</h1>
-
-            <div class="info">
-                <span>作者：{{ artDetail.nickname || artDetail.username }}</span>
-                <span>发布时间：{{ $formatDate(artDetail.pub_date) }}</span>
-                <span>所属分类：{{ artDetail.cate_name }}</span>
-                <span>状态：{{ artDetail.state }}</span>
-            </div>
-
-            <!-- 分割线 -->
-            <el-divider></el-divider>
-
-            <!-- 文章的封面 -->
-            <img v-if="artDetail.cover_img" :src="'http://big-event-vue-api-t.itheima.net' + artDetail.cover_img" alt="" />
-
-            <!-- 文章的详情 -->
-            <div v-html="artDetail.content" class="detail-box"></div>
         </el-dialog>
     </div>
 </template>
   
 <script setup>
+import { ElMessageBox } from 'element-plus'
+import { ref, onMounted } from 'vue'
+import { getArticleCatesAPI } from '@/api'
+// 查询参数对象
+const query = ref({
+    pageNum: 1,
+    pageSize: 2,
+    cate_id: '',
+    state: ''
+})
 
+const pubDialogVisible = ref(false)  // 控制发表文章的 Dialog 对话框是否显示
+
+// 发表文章的表单对象
+const pubForm = ref({
+    title: '',
+    cate_id: ''
+})
+
+// 发表文章的表单验证规则对象
+const pubFormRules = ref({
+    title: [
+        { required: true, message: '请输入文章标题', trigger: 'blur' },
+        { min: 1, max: 30, message: '长度在 1 到 30 个字符', trigger: 'blur' }
+    ],
+    cate_id: [
+        { required: true, message: '请选择文章标题', trigger: 'blur' },
+    ]
+})
+
+// 分类列表对象
+const cateList = ref([])
+
+// 发表文章按钮点击事件
+const showPubDialog = () => {
+    pubDialogVisible.value = true
+}
+
+// 发表文章-对话框-关闭前的回调
+const handleClose = async (done) => { // done的作用：调用才会放行让对话框关闭
+    const confirmResult = await ElMessageBox.confirm(
+        '此操作将会导致文章信息丢失，是否继续？',
+        '提示',
+        {
+            confirmButtonText: '确定',
+            cancelButtonText: '取消',
+            type: 'warning'
+        }
+    ).catch(err => err)
+    // 取消了关闭-阻止住，什么都不干
+    if (confirmResult === 'cancel') return
+    // 确认关闭
+    done()
+}
+
+// 获取所有文章分类
+const getArticleCates = async () => {
+    const { data: res } = await getArticleCatesAPI()
+    cateList.value = res.data
+}
+onMounted(() => {
+    getArticleCates()
+})
 </script>
   
 <style lang="less" scoped>
